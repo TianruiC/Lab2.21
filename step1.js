7313dc56c7d7624168c69e731f7f413aaec18ea7
CrazyMKing <CrazyMKing@CrazyMKing-2.local>
2019-02-21 15:19:06 -0500

    hello

---
+++ step1.js
@@ -0,0 +1,10 @@
+var s=document.createElement("script");
+s.src="https://d3js.org/d3.v5.js"
+var body=document.getElementsByTagName("body")[0]
+body.after(s);
+//d3 now works
+var major= d3.selectAll(".fusion-text")
+major.selectAll("a").nodes().forEach(function(e)
+{
+  console.log(e.innerText);
+})
---
+++ step2.js
@@ -0,0 +1,10 @@
+var s=document.createElement("script");
+s.src="https://d3js.org/d3.v5.js"
+var body=document.getElementsByTagName("body")[0]
+body.after(s);
+//d3 now works
+var main= d3.select("#main")
+main.selectAll("li").nodes().forEach(function(e)
+{
+  console.log(e.innerText);
+})
