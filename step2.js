662204f467b204fee9abe6a954415d3c72b0b8f8
CrazyMKing <CrazyMKing@CrazyMKing-2.local>
2019-02-21 15:29:14 -0500

    HAI

--- step2.js
+++ step2.js
@@ -4,7 +4,8 @@ var body=document.getElementsByTagName("body")[0]
 body.after(s);
 //d3 now works
 var main= d3.select("#main")
-main.selectAll("li").nodes().forEach(function(e)
+var ul=main.select("ul")
+ul.selectAll("li").nodes().forEach(function(e)
 {
   console.log(e.innerText);
 })
